4fdf42852e1b2e3b678c2c8ba1b4b6da69809a1c
Ben H <ben.helfstein@icloud.com>
2026-09-18 12:34:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,3 +4,12 @@ public class Main {
       System.out.println("Hello World");
    }
 }
+
+//algorithum - step be step instructions to acomplish a task
+//pseudocode - simplified code to outline algorythums
+//sequencing - the order steps figuring out
+
+/*
+Java is class based oop language, has a compiler
+Javascript is simpaler, used for web dev
+*/
